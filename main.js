9f3b3761ab5e3f734289d05c43af96e375be3045
Yngwarr <k.yngvarr@gmail.com>
2017-08-05 15:38:30 +0700

    MVP. Needs some style and minor improvements.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const VOCAB_FILE = './vocab.txt';
 
+// all the possible questions for all the cats
+let DATA = [];
+// questions to answer (idx)
+let QS = [];
+// list of failed questions (idx)
+let FAILED = [];
+
 function init() {
 	reqData(VOCAB_FILE);
 }
@@ -20,13 +27,12 @@ function reqData(url) {
 }
 
 function procJson(txt) {
-	let data = [];
 	let strs = txt.split('\n').filter((x) => {
 		return x !== '';
 	});
 	strs.forEach((s) => {
 		if (s[0] === '#') {
-			data.push({
+			DATA.push({
 				'n': s.substr(3),
 				'qs': []
 			});
@@ -36,12 +42,94 @@ function procJson(txt) {
 			// get rid of last space
 			w = w.substr(0, w.length-1);
 			p = p.substr(0, p.length-2);
-			data[data.length-1]['qs'].push({
+			DATA[DATA.length-1]['qs'].push({
 				'w': w,
 				'p': p,
 				'def': def
 			});
 		}
 	});
-	console.log(data);
+	console.log(DATA);
+	renderList();
+}
+
+function renderList() {
+	let ul = document.getElementById('cat-list');
+
+	DATA.forEach((cat, idx) => {
+		let li = document.createElement('li');
+		li.innerHTML = `${cat.n} `
+			+ `<i>(${cat.qs.length})</i>`;
+		li.dataset['idx'] = idx;
+		li.addEventListener('click', startQuiz);
+		ul.appendChild(li);
+	});
+
+	document.getElementById('cat-layout')
+		.classList.remove('hidden');
+}
+
+function startQuiz(evt) {
+	let cat_idx = parseInt(evt.target.dataset['idx']);
+	QS = genSeq(DATA[cat_idx]['qs'].length);
+	FAILED = [];
+
+	// write the name of the category
+	document.getElementById('cat-name').innerHTML
+		= DATA[cat_idx]['n'];
+	document.getElementById('cnt-curr').innerHTML = '1';
+	document.getElementById('cnt-sum').innerHTML =
+		`${DATA[cat_idx].qs.length}`;
+
+	nextQuest(cat_idx);
+	//document.getElementById('answer').addEventListener(
+	//'change', () => {
+	document.querySelector('form').addEventListener(
+	'submit', (e) => {
+		e.preventDefault();
+		checkAnswer(cat_idx);
+		return false;
+	});
+
+	document.getElementById('cat-layout')
+		.classList.add('hidden');
+	document.getElementById('quest-layout')
+		.classList.remove('hidden');
+}
+
+function checkAnswer(cat_idx) {
+	let input = document.getElementById('answer');
+	let ans = input.value;
+	let correct = DATA[cat_idx].qs[QS[QS.length - 1]].w;
+	let span = document.getElementById('correct');
+	span.innerHTML = ans.indexOf(correct) < 0
+		? `🗴 ${correct}` : `🗸 ${correct}`;
+	input.value = '';
+	if (QS !== []) {
+		QS.pop();
+		nextQuest(cat_idx);
+	}
+}
+
+function nextQuest(cat_idx) {
+	let q_idx = QS[QS.length-1];
+	let p = document.getElementById('part');
+	let def = document.getElementById('definition');
+
+	document.getElementById('cnt-curr').innerHTML =
+		`${DATA[cat_idx].qs.length - QS.length + 1}`;
+	
+	p.innerHTML = DATA[cat_idx].qs[q_idx].p;
+	def.innerHTML = DATA[cat_idx].qs[q_idx].def;
+}
+
+function genSeq(len) {
+	let arr = [...Array(len).keys()];
+	for (let i = len - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		let temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	return arr;
 }
